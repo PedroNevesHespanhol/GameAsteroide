32b1f244a9f012d5bb128e2273cf16314d271ac1
PedroNevesHespanhol <88465619+PedroNevesHespanhol@users.noreply.github.com>
2022-09-21 20:24:03 -0300

    Commit

---
+++ login.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('.login-input');
+const senha = document.querySelector('.login-senha');
+const button = document.querySelector('.login-button');
+const form = document.querySelector('.login-form');
+
+const validateInput = ({ target}) => {
+    if(target.value.length > 4){
+        button.removeAttribute('disabled');
+        return;
+    }
+
+    button.setAttribute('disabled', '');
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('jogador', input.value);
+    window.location = 'game.html';
+}
+
+senha.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
